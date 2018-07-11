f9e6718aed4d1ad5bf525cae42659006514fd233
ViktoriiaBilaniuk <vika.bilaniuk@gmail.com>
2018-07-11 17:35:05 +0300

    start working with login page

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard.component';
 
 export const DASHBOARD_ROUTES: Routes = [
   { path: '', component: DashboardComponent }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { TestComponent } from './test/test.component';
-import { TitleComponent } from './dashboard/home/title/title.component';
-import { MnFullpageModule } from 'ngx-fullpage';
-import { HomeComponent } from './dashboard/home/home.component';
+import { TitleComponent } from './home/title/title.component';
+import { HomeComponent } from './home/home.component';
+import { HowItWorksComponent } from './home/how-it-works/how-it-works.component';
+import { StartSearchComponent } from './home/start-search/start-search.component';
 
 @NgModule({
   imports: [
     SharedModule,
     DashboardRoutingModule,
-    MnFullpageModule.forRoot()
   ],
   declarations: [
     DashboardComponent,
     TestComponent,
     TitleComponent,
     HomeComponent,
+    HowItWorksComponent,
+    StartSearchComponent,
 
   ],
   providers: [
